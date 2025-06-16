13d6e3a2b115e3ff16b9d9d73da1df0f45ae45e5
Adrián Ramos <116114725+AMarceloRamos@users.noreply.github.com>
2025-06-16 13:55:41 -0400

    Update page.tsx
    Se eliminaron elementos que hacían referencias a ejemplos que ya no eran necesarios

--- nextjs-dasboard/app/page.tsx
+++ nextjs-dasboard/app/page.tsx
@@ -51,9 +51,7 @@ export default function Page() {
             alt='Screenshots of the dashboard projects mobileversion'
           />
         </div>
-        <h1 className="text-blue-500">hola soy un texto de color azul</h1>
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"></div>
-        <div className={styles.shape} />
+    
       </div>
       
     </main>
